84b2dff53960487042e96e80e514a0a56a8835d3
bashidagha <farshidk49@gmail.com>
2025-12-07 13:46:54 +0330

    add recent blogs and about me to home

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyMarkdownBlog.Services;
+using MyMarkdownBlog.Models;
+using System.Collections.Generic;
 
 namespace MyMarkdownBlog.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
-    {
+    private readonly IBlogService _blogService;
 
+    public IndexModel(IBlogService blogService)
+    {
+        _blogService = blogService;
+    }
+
+    public List<BlogPost> RecentPosts { get; set; } = new();
+    public string AboutMeContent { get; set; } = string.Empty;
+
+    public async Task OnGetAsync()
+    {
+        // Get recent blog posts (limit to 3 most recent)
+        var allPosts = await _blogService.GetAllPostsAsync();
+        RecentPosts = allPosts.Take(3).ToList();
+
+        // Set about me content
+        AboutMeContent = "Welcome to my personal blog! I'm a passionate developer who loves sharing knowledge and experiences about technology, programming, and software development. Here you'll find articles, tutorials, and insights on various topics in the tech world.";
     }
 }
